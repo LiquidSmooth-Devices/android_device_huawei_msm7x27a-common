7b84e2db77a3145da5cb73c4e414abfbb8aba345
Y300-0100 <bosko.brajovich@gmail.com>
2014-09-03 11:33:43 +0200

    Update msm7x27a.mk

--- msm7x27a.mk
+++ msm7x27a.mk
@@ -55,31 +55,6 @@ PRODUCT_PACKAGES += \
     gps.msm7x27a
 #    lights.msm7x27a
 
-ifeq ($(BOARD_HAVE_NFC),true)
-
-PRODUCT_COPY_FILES += \
-    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/base/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-
-# NFC Support
-PRODUCT_PACKAGES += \
-    libnfc \
-    libnfc_jni \
-    Nfc \
-    Tag \
-    com.android.nfc_extras
-
-# NFCEE access control
-ifeq ($(TARGET_BUILD_VARIANT),user)
-    NFCEE_ACCESS_PATH := device/huawei/msm7x27a-common/configs/nfcee_access.xml
-else
-    NFCEE_ACCESS_PATH := device/huawei/msm7x27a-common/configs/nfcee_access_debug.xml
-endif
-PRODUCT_COPY_FILES += \
-    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
-
-endif # BOARD_HAVE_NFC
-
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory \
     hwmac \
